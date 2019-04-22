b6518aed6a7e17193ca9fd3fec7164d74e804ac8
RicardoLjy <824654793@qq.com>
2019-04-22 17:36:59 +0800

    远程dev

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>hello world</h1>'
+
+r'''
+    动态路由参数：name需要动态输入
+'''
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,%s</h1>' % name
+
 if __name__ == '__main__':
     app.run(debug=True)
-teggfgf
+
